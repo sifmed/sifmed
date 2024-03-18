714b88f7a4aa1e538fc75b31b884599961f4f3ce
sif <148813940+sifmed@users.noreply.github.com>
2024-03-18 22:14:52 +0100

    Create menuActivity.java

---
+++ activity_main/menuActivity.java
@@ -0,0 +1,14 @@
+package com.example.gsbrv2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class menuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
